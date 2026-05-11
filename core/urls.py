4bd9a0027b13b2415ade96aacb486e7af6692b86
JP-SOMBRA <pablo.hernandez.censa@gmail.com>
2026-05-10 21:17:47 -0400

    Backend: Implementación de rutas API REST para inventario #12

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('status/', views.status_check, name='status_check'),
     path('login/', views.login_view, name='login_view'),
     path('cambiar-clave/', views.cambiar_clave_view, name='cambiar_clave_view'),
+    
+    # Módulo de Inventario
+    path('inventario/criticos/', views.inventario_criticos, name='inventario_criticos'),
+    path('inventario/', views.inventario_list_create, name='inventario_list_create'),
+    path('inventario/<str:codigo>/', views.inventario_detail, name='inventario_detail'),
 ]
-
