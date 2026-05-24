aff09d97d6ccf894e09b3281ed94413ab4503800
Walter <144754208+wheeer@users.noreply.github.com>
2026-05-24 18:06:03 -0400

    refactor: reestructurar views de core en paquete modular por subdominios (#44)
    reestructuración ok

---
+++ core/views/__init__.py
@@ -0,0 +1,8 @@
+from .auth import status_check, login_view, cambiar_clave_view
+from .remuneraciones import (
+    calcular_remuneraciones,
+    obtener_pdf_liquidacion,
+    obtener_liquidacion_empleado,
+    obtener_remuneraciones,
+    obtener_horas_extra
+)
